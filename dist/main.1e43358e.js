fde1819d9796d74a3e08a61308925e8e099bd016
shawn <shawnantonucci2018@gmail.com>
2019-02-02 15:24:11 -0800

    adding animations for player

--- dist/main.1e43358e.js
+++ dist/main.1e43358e.js
@@ -779,6 +779,8 @@ exports.Level01 = void 0;
 
 var _CST = require("../CST");
 
+var _CharacterSprite = require("../CharacterSprite");
+
 var _Sprite = require("../Sprite");
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
@@ -815,64 +817,65 @@ function (_Phaser$Scene) {
   _createClass(Level01, [{
     key: "preload",
     value: function preload() {
-      this.load.spritesheet('player', 'assets/sprite/player.png', {
-        frameWidth: 16,
-        frameHeight: 28
-      }); // this.load.image('target', 'assets/spite/ball.png');
-      // this.load.image('background', 'assets/image/underwater1.png');
-      //player character animations
-      // this.anims.create({
-      //   key: "left",
-      //   frameRate: 10,
-      //   frames: this.anims.generateFrameNumbers("player", {
-      //     start: 9,
-      //     end: 17
-      //   })
-      // });
-      // this.anims.create({
-      //   key: "down",
-      //   frameRate: 10,
-      //   frames: this.anims.generateFrameNumbers("player", {
-      //     start: 18,
-      //     end: 26
-      //   })
-      // });
-      // this.anims.create({
-      //   key: "up",
-      //   frameRate: 10,
-      //   frames: this.anims.generateFrameNumbers("player", {
-      //     start: 0,
-      //     end: 8
-      //   })
-      // });
-      // this.anims.create({
-      //   key: "right",
-      //   frameRate: 10,
-      //   frames: this.anims.generateFrameNumbers("player", {
-      //     start: 27,
-      //     end: 35
-      //   })
-      // });
+      this.load.atlas('player', 'assets/atlas/player.png', 'assets/atlas/player.json');
     }
   }, {
     key: "create",
     value: function create() {
       // Create world bounds
-      this.physics.world.setBounds(0, 0, 800, 600); // Add background, player, and reticle sprites
-      // var background = this.add.image(800, 600, 'background');
+      this.physics.world.setBounds(0, 0, 800, 600); // Create player sprite
 
-      this.player = this.physics.add.sprite(400, 300, 'player').setScale(1); // reticle = this.physics.add.sprite(800, 700, 'target');
-      // Set image/sprite properties
-      // background.setOrigin(0.5, 0.5).setDisplaySize(800, 600);
-
-      this.player.setOrigin(0.5, 0.5).setDisplaySize(100, 120).setCollideWorldBounds(true).setDrag(500, 500); // reticle.setOrigin(0.5, 0.5).setDisplaySize(25, 25).setCollideWorldBounds(true);
-
+      this.player = this.physics.add.sprite(100, 450, "player", 'HC_Humans1A_56.png').setScale(2);
       window.player = this.player; //set smaller hitbox
 
       this.player.setSize(20, 34).setOffset(-3, -3);
       this.player.setCollideWorldBounds(true);
-      this.keyboard = this.input.keyboard.addKeys("W, A, S, D"); // Set camera zoom
-      // this.cameras.main.zoom = 0.5;
+      this.keyboard = this.input.keyboard.addKeys("W, A, S, D"); //player character animations
+
+      this.anims.create({
+        key: "down",
+        frameRate: 5,
+        frames: this.anims.generateFrameNames("player", {
+          prefix: 'HC_Humans1A_',
+          suffix: '.png',
+          start: 55,
+          end: 57,
+          zeroPad: 2
+        })
+      });
+      this.anims.create({
+        key: "up",
+        frameRate: 5,
+        frames: this.anims.generateFrameNames("player", {
+          prefix: 'HC_Humans1A_',
+          suffix: '.png',
+          start: 91,
+          end: 93,
+          zeroPad: 2
+        })
+      });
+      this.anims.create({
+        key: "left",
+        frameRate: 5,
+        frames: this.anims.generateFrameNames("player", {
+          prefix: 'HC_Humans1A_',
+          suffix: '.png',
+          start: 67,
+          end: 69,
+          zeroPad: 2
+        })
+      });
+      this.anims.create({
+        key: "right",
+        frameRate: 5,
+        frames: this.anims.generateFrameNames("player", {
+          prefix: 'HC_Humans1A_',
+          suffix: '.png',
+          start: 79,
+          end: 81,
+          zeroPad: 2
+        })
+      });
     }
   }, {
     key: "update",
@@ -903,20 +906,21 @@ function (_Phaser$Scene) {
         if (this.keyboard.W.isUp && this.keyboard.S.isUp) {
           //not pressing y movement
           this.player.setVelocityY(0);
-        } // if (this.player.body.velocity.x > 0) {
-        //   //moving right
-        //   this.player.play("right", true);
-        // } else if (this.player.body.velocity.x < 0) {
-        //   //moving left
-        //   this.player.anims.playReverse("left", true);
-        // } else if (this.player.body.velocity.y < 0) {
-        //   //moving up
-        //   this.player.play("up", true);
-        // } else if (this.player.body.velocity.y > 0) {
-        //   //moving down
-        //   this.player.play("down", true);
-        // }
+        }
 
+        if (this.player.body.velocity.x > 0) {
+          //moving right
+          this.player.play("right", true);
+        } else if (this.player.body.velocity.x < 0) {
+          //moving left
+          this.player.play("left", true);
+        } else if (this.player.body.velocity.y < 0) {
+          //moving up
+          this.player.play("up", true);
+        } else if (this.player.body.velocity.y > 0) {
+          //moving down
+          this.player.play("down", true);
+        }
       }
     }
   }]);
@@ -925,7 +929,7 @@ function (_Phaser$Scene) {
 }(Phaser.Scene);
 
 exports.Level01 = Level01;
-},{"../CST":"src/CST.js","../Sprite":"src/Sprite.js"}],"src/main.js":[function(require,module,exports) {
+},{"../CST":"src/CST.js","../CharacterSprite":"src/CharacterSprite.js","../Sprite":"src/Sprite.js"}],"src/main.js":[function(require,module,exports) {
 "use strict";
 
 var _LoadScene = require("./scenes/LoadScene");
--- src/scenes/Level01.js
+++ src/scenes/Level01.js
@@ -1,4 +1,5 @@
 import { CST } from "../CST";
+import { CharacterSprite } from "../CharacterSprite";
 import { Sprite } from "../Sprite";
 
 export class Level01 extends Phaser.Scene {
@@ -7,72 +8,71 @@ export class Level01 extends Phaser.Scene {
   }
 
   preload() {
-    this.load.spritesheet('player', 'assets/sprite/player.png', { frameWidth: 16, frameHeight: 28 });
-    // this.load.image('target', 'assets/spite/ball.png');
-    // this.load.image('background', 'assets/image/underwater1.png');
-
-
-    //player character animations
-    // this.anims.create({
-    //   key: "left",
-    //   frameRate: 10,
-    //   frames: this.anims.generateFrameNumbers("player", {
-    //     start: 9,
-    //     end: 17
-    //   })
-    // });
-    // this.anims.create({
-    //   key: "down",
-    //   frameRate: 10,
-    //   frames: this.anims.generateFrameNumbers("player", {
-    //     start: 18,
-    //     end: 26
-    //   })
-    // });
-    // this.anims.create({
-    //   key: "up",
-    //   frameRate: 10,
-    //   frames: this.anims.generateFrameNumbers("player", {
-    //     start: 0,
-    //     end: 8
-    //   })
-    // });
-    // this.anims.create({
-    //   key: "right",
-    //   frameRate: 10,
-    //   frames: this.anims.generateFrameNumbers("player", {
-    //     start: 27,
-    //     end: 35
-    //   })
-    // });
-
+    this.load.atlas('player', 'assets/atlas/player.png', 'assets/atlas/player.json')
   }
 
   create() {
     // Create world bounds
     this.physics.world.setBounds(0, 0, 800, 600);
 
-    // Add background, player, and reticle sprites
-      // var background = this.add.image(800, 600, 'background');
-    this.player = this.physics.add.sprite(400, 300, 'player').setScale(1);
-      // reticle = this.physics.add.sprite(800, 700, 'target');
+    // Create player sprite
+    this.player = this.physics.add.sprite(100, 450, "player", 'HC_Humans1A_56.png').setScale(2);
 
-    // Set image/sprite properties
-      // background.setOrigin(0.5, 0.5).setDisplaySize(800, 600);
-    this.player.setOrigin(0.5, 0.5).setDisplaySize(100, 120).setCollideWorldBounds(true).setDrag(500, 500);
-      // reticle.setOrigin(0.5, 0.5).setDisplaySize(25, 25).setCollideWorldBounds(true);
+    window.player = this.player;
 
-
-      window.player = this.player;
-
-          //set smaller hitbox
+    //set smaller hitbox
     this.player.setSize(20, 34).setOffset(-3, -3);
     this.player.setCollideWorldBounds(true);
     this.keyboard = this.input.keyboard.addKeys("W, A, S, D");
 
+    //player character animations
+    this.anims.create({
+      key: "down",
+      frameRate: 5,
+      frames: this.anims.generateFrameNames("player", {
+        prefix: 'HC_Humans1A_',
+        suffix: '.png',
+        start: 55,
+        end: 57,
+        zeroPad: 2
+      }),
+    });
+    this.anims.create({
+      key: "up",
+      frameRate: 5,
+      frames: this.anims.generateFrameNames("player", {
+        prefix: 'HC_Humans1A_',
+        suffix: '.png',
+        start: 91,
+        end: 93,
+        zeroPad: 2
+      }),
+    });
+    this.anims.create({
+      key: "left",
+      frameRate: 5,
+      frames: this.anims.generateFrameNames("player", {
+        prefix: 'HC_Humans1A_',
+        suffix: '.png',
+        start: 67,
+        end: 69,
+        zeroPad: 2
+      }),
+    });
+    this.anims.create({
+      key: "right",
+      frameRate: 5,
+      frames: this.anims.generateFrameNames("player", {
+        prefix: 'HC_Humans1A_',
+        suffix: '.png',
+        start: 79,
+        end: 81,
+        zeroPad: 2
+      }),
+    });
 
-    // Set camera zoom
-    // this.cameras.main.zoom = 0.5;
+
+
   }
 
   update(time, delta) {
@@ -103,19 +103,19 @@ export class Level01 extends Phaser.Scene {
         this.player.setVelocityY(0);
       }
 
-      // if (this.player.body.velocity.x > 0) {
-      //   //moving right
-      //   this.player.play("right", true);
-      // } else if (this.player.body.velocity.x < 0) {
-      //   //moving left
-      //   this.player.anims.playReverse("left", true);
-      // } else if (this.player.body.velocity.y < 0) {
-      //   //moving up
-      //   this.player.play("up", true);
-      // } else if (this.player.body.velocity.y > 0) {
-      //   //moving down
-      //   this.player.play("down", true);
-      // }
+      if (this.player.body.velocity.x > 0) {
+        //moving right
+        this.player.play("right", true);
+      } else if (this.player.body.velocity.x < 0) {
+        //moving left
+        this.player.play("left", true);
+      } else if (this.player.body.velocity.y < 0) {
+        //moving up
+        this.player.play("up", true);
+      } else if (this.player.body.velocity.y > 0) {
+        //moving down
+        this.player.play("down", true);
+      }
     }
   }
 }
